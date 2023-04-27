8d86513fdb0c19a4c028cc85738e2721f0ba8c27
wbc0410 <445124965@qq.com>
2023-04-27 08:18:16 +0800

    third commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,7 @@ public class Main {
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
         System.out.printf("Hello and welcome2!");
+        System.out.printf("Hello and welcome3!");
 
         // Press ⌃R or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
