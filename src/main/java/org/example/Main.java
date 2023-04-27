5374a3b7120c863ed17ced38f73d642cbd8b5b9a
wbc0410 <445124965@qq.com>
2023-04-27 08:09:45 +0800

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ public class Main {
         // Press ⌥⏎ with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome2!");
 
         // Press ⌃R or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
